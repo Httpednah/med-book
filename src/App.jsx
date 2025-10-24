f5ad556529c4680ee9d9c7c89dc7887c6d9ae718
Ednah wairimu kimani <kimaniednah@gmail.com>
2025-10-24 10:50:06 +0300

    Added and updated Home component + routing changes

--- src/App.jsx
+++ src/App.jsx
@@ -2,30 +2,23 @@
 import { Routes, Route } from "react-router-dom";
 import { Toaster } from "react-hot-toast";
 import Layout from "./components/Layout";
+import Home from "./components/Home";
 import DoctorsList from "./components/DoctorsList";
 import BookAppointmentForm from "./components/BookAppointmentForm";
 import AppointmentsList from "./components/AppointmentsList";
 
-
-
-
 function App() {
   return (
     <>
-    <div className="p-6">
-      <h1 className="text-2xl font-bold mb-4">Available Doctors</h1>
-      <DoctorsList />
       <Toaster position="top-right" />
-      <BookAppointmentForm/>
       <Routes>
         <Route element={<Layout />}>
-          <Route path="/" element={<DoctorsList />} />
+          <Route path="/" element={<Home />} />
+          <Route path="/doctors" element={<DoctorsList />} />
           <Route path="/book" element={<BookAppointmentForm />} />
           <Route path="/appointments" element={<AppointmentsList />} />
         </Route>
       </Routes>
-    </div>
-     
     </>
   );
 }
